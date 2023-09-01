89798f738a3c084b5c3e2ccafdea8e3acafa72fa
chimuka moonde <chimukamoonde@gmail.com>
2023-09-01 08:43:43 +0200

    minor adjustment

--- 4-natours/after-section-14/app.js
+++ 4-natours/after-section-14/app.js
@@ -48,27 +48,34 @@ app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+	app.use(morgan('dev'));
 }
 
 // Limit requests from same API
 const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
 
 // Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
 app.post(
-  '/webhook-checkout',
-  bodyParser.raw({ type: 'application/json' }),
-  bookingController.webhookCheckout
+	'/webhook-checkout',
+	bodyParser.raw({
+		type: 'application/json'
+	}),
+	bookingController.webhookCheckout
 );
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({
+	limit: '10kb'
+}));
+app.use(express.urlencoded({
+	extended: true,
+	limit: '10kb'
+}));
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
@@ -79,25 +86,25 @@ app.use(xss());
 
 // Prevent parameter pollution
 app.use(
-  hpp({
-    whitelist: [
-      'duration',
-      'ratingsQuantity',
-      'ratingsAverage',
-      'maxGroupSize',
-      'difficulty',
-      'price'
-    ]
-  })
+	hpp({
+		whitelist: [
+			'duration',
+			'ratingsQuantity',
+			'ratingsAverage',
+			'maxGroupSize',
+			'difficulty',
+			'price'
+		]
+	})
 );
 
 app.use(compression());
 
 // Test middleware
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  // console.log(req.cookies);
-  next();
+	req.requestTime = new Date().toISOString();
+	// console.log(req.cookies);
+	next();
 });
 
 // 3) ROUTES
@@ -108,7 +115,7 @@ app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
